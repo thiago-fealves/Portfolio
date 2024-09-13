0a31c38d7d2b80cdcc61a334787970cfe37ba5a6
Thiago Felipe Alves do Carmo <thiagofelipealves13@gmail.com>
2024-09-13 12:37:22 -0300

    v1.0.5
    minor text changes

--- public/Components/mainbanner.tsx
+++ public/Components/mainbanner.tsx
@@ -29,7 +29,7 @@ const MainBanner = () => {
             {portugues ? "" : " Developer"}
           </Title>
           <Title subtitle={true} customcls={styles.pentester}>
-            {portugues ? "& OffSec Enthusiast" : "& OffSec Enthusiast"}
+            {portugues ? "& Entusiasta OffSec" : "& OffSec Enthusiast"}
           </Title>
           <Paragraph customcls={styles.paragraph}>
             {portugues
